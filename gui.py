bd1f0f99d4267cf19a3d7bf46a10e446ea9c7543
retard1377 <misha.domanov@bk.ru>
2019-06-25 02:14:29 +0300

    Update gui.py

--- gui.py
+++ gui.py
@@ -3,6 +3,25 @@ import tkinter as tk
 root = tk.Tk()
 root.geometry('1075x665')
 root.title('PyTemp')
-tk.Button(text='New', command='').place(x=20,y=500)
-
+keyList = []
+mainmenu = tk.Menu(root) 
+root.config(menu=mainmenu) 
+def createmail():
+    api = postshiftapi.Api()
+    MailData = api.CreateNew()
+    print(MailData)
+    mail = MailData.get('email')
+    key = MailData.get('key')
+    root.title('PyTemp - '+mail)
+    keyList.append(key)
+    del api, MailData, key, mail
+def delmail():
+    api = postshiftapi.Api()
+    api.DeleteMail(keyList[-1])
+    root.title('PyTemp')
+    del keyList[-1], api
+filemenu = tk.Menu(mainmenu, tearoff=0)
+filemenu.add_command(label="Создать", command = createmail)
+filemenu.add_command(label="Удалить", command = delmail)
+mainmenu.add_cascade(label="Почта", menu=filemenu)
 root.mainloop()
